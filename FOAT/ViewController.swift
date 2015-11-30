102beb8a0a4f1f3928ce7eaf9cc847581260b447
任然 <rou_108@icloud.com>
2015-11-30 23:19:13 +0800

    Initial Commit

---
+++ FOAT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FOAT
+//
+//  Created by stiller on 15/11/30.
+//  Copyright © 2015年 stiller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FOATTests/FOATTests.swift
@@ -0,0 +1,36 @@
+//
+//  FOATTests.swift
+//  FOATTests
+//
+//  Created by stiller on 15/11/30.
+//  Copyright © 2015年 stiller. All rights reserved.
+//
+
+import XCTest
+@testable import FOAT
+
+class FOATTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
